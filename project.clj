681f5b58bcd4e2f3c6b079db1c49e883488e3bd1
Gary Fredericks <fredericksgary@gmail.com>
2016-08-01 22:14:30 -0500

    lein cljfmt

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/gfredericks/schpec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]]
+  :plugins [[lein-cljfmt "0.5.3"]])

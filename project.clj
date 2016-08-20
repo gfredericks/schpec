ae599442b8d16deb9af382d960823eda9f15c7cb
Gary Fredericks <fredericksgary@gmail.com>
2016-08-20 17:12:19 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/schpec "0.1.0-SNAPSHOT"
+(defproject com.gfredericks/schpec "0.1.0"
   :description "A utility library for clojure.spec"
   :url "https://github.com/gfredericks/schpec"
   :license {:name "Eclipse Public License"
